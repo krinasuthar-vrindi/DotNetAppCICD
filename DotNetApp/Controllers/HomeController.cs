6d9d420eecb7394cca24ab95a24584d72613b037
Krina Suthar <krina.suthar@vrindimail.com>
2025-06-18 15:52:02 +0530

    Auto application pool recycle changes

--- DotNetApp/Controllers/HomeController.cs
+++ DotNetApp/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace DotNetApp.Controllers
 
         public IActionResult Index()
         {
-            _logger.LogInformation("Index View loading on 18 6 2025");
+            _logger.LogInformation("Index View loading on 18 6 2025 3");
             return View();
         }
 
