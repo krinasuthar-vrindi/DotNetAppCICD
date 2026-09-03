913a71aeace3a98eabf0df595698ea7fb99432ef
Krina Suthar <krina.suthar@vrindimail.com>
2026-09-03 11:20:54 +0530

    comment added

--- DotNetApp/Controllers/HomeController.cs
+++ DotNetApp/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace DotNetApp.Controllers
             _logger = logger;
         }
 
-        //comment added - 01-09-2026
+        //comment added - 01-09-2026 for index page
         public IActionResult Index()
         {
             _logger.LogInformation("Index View loading at {Time}ngfh gfhfgh", DateTime.UtcNow);
